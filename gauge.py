48a2c2ed3b73d50c80f27c78fd1c692b570da32a
coltdb45 <142354510+coltdb45@users.noreply.github.com>
2023-09-06 08:54:20 -0500

    Add files via upload

--- gauge.py
+++ gauge.py
@@ -1,18 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import math
+
+
+colorslist = ['#22751b','#17fc03','#f8fc03','#fc8803','#fc0303']
 
 ### Defines the Basic Formating of the Dial
 fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
 ax.set_rmax(2)
 
-ax.set_rlabel_position(-22.5)  # Move radial labels away from plotted line
 ax.set_title("TFOM", va='bottom')
+ax.set_rticks([])
+# ax.set_xticks([])  
 
-### Starts Calculating Where to Draw the Line
-ypoints = np.array([math.sqrt(2)/-2,math.sqrt(2)/-2])
 
-plt.plot(ypoints, linestyle = '-')
+ax.vlines(5*np.pi/4,0,2) # Angle, Starting Point, Radius
+
+plt.grid('off')
 plt.show()
 
 
