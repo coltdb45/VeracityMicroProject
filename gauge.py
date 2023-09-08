5b08d5ac447ab2a75cd3cf0b127982ff032d4c02
coltdb45 <142354510+coltdb45@users.noreply.github.com>
2023-09-08 14:01:05 -0500

    Add files via upload

--- gauge.py
+++ gauge.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-colorslist = ['#22751b','#17fc03','#f8fc03','#fc8803','#fc0303']
+6
+colorslist = ['#fc0303','#fc8803','#f8fc03','#17fc03' ,'#22751b' ]
 
 ### Defines the Basic Formating of the Dial
 fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
@@ -13,9 +13,20 @@ ax.set_rticks([])
 # ax.set_xticks([])  
 
 
-ax.vlines(5*np.pi/4,0,2) # Angle, Starting Point, Radius
 
-plt.grid('off')
+angle =  float(input("TFOM - "))
+
+angle = (-19.286 * angle ) + 244.292
+
+
+angle = angle * (np.pi / 180)
+
+
+ax.bar(x=[-0.78,0.156,1.092,2.028,2.964], width=0.936, height = 1.25, bottom = 2.2, color = colorslist, align = "edge")
+ax.vlines(angle,0,2) # Angle, Starting Point, Radius
+
+plt.axis('off')
+plt.grid(False)
 plt.show()
 
 
